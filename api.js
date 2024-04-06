0617c5c601f08e7b7449b1340a98136f003c2423
kopytab <bryc3.k@gmail.com>
2024-04-06 18:03:36 -0400

    fixed

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
 
 
 const getPokeData = async () => {
+    let pokemonName = document.getElementById('pokemonName').value;
     const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
     const data = await resp.json();
 
-    document.getElementById('pokemonName').textContent = data.name;
-    document.getElementById('pokemonImage').src = data.sprites.front_shiny;
+    document.getElementById('pokeName').innerHTML = data.name;
+    document.getElementById('pokeImage').src = data.sprites.front_shiny;
 
 }
